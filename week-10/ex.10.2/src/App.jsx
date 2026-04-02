d901a40ec47732ac6bf1ba0dbebcf9943b733b76
ckoutianov <ckoutianov@mcad.edu>
2026-04-02 13:31:10 -0700

    setup react app

---
+++ week-10/ex.10.2/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link></Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+      </ul>
+    </nav>
+     Hello World
+    </>
+  )
+}
+
+export default App
