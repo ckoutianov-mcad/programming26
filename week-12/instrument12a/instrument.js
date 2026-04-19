b3d8fa562fab891dd5d6d16e53b43a7649ec2c11
ckoutianov <ckoutianov@mcad.edu>
2026-04-19 16:38:10 -0700

    keys update instrument.js

--- week-12/instrument12a/instrument.js
+++ week-12/instrument12a/instrument.js
@@ -1,32 +1,32 @@
 let osc, env;
-let notes = [50, 52, 54, 55, 57, 59, 61, 62];//notes array
-let c = [0, 30, 60, 90, 120, 240, 270, 300];
+let notes = [50, 52, 54, 55, 57, 59, 61, 62];//notes
+let c = [0, 30, 60, 90, 160, 240, 270, 300]; //color
+let keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k']; //keys pressed
 
 
 function setup() {
     createCanvas(800, 300);
     colorMode(HSB);
     env = new p5.Envelope();
-
     env.setADSR(0.01, 0.1, 1, 0.25);
-
     osc = new p5.Oscillator('triangle');
     osc.start();
     osc.amp(env);
 }
 
 function draw() {
-    background(220, 0, 80);
-    osc.freq(midiToFreq(notes[6]));//different notes
-    //keys to play notes
-    for(let i = 0; i < 8; i++) {
-        fill(c[4], 100, 100);
-        rect(30 + (i*90), 50, 80, 80);
-        noLoop();
+  background(16, 107, 255);
+  //keys to play notes
+  for (let i = 0; i < 8; i++) {
+    fill(0, 0, 100); //default key colors
+    if (keyIsPressed && key == keys[i]) {
+      fill(c[i], 100, 100);
+      osc.freq(midiToFreq(notes[i])); //different notes
     }
-    console.log(notes);
+    rect(30 + i * 90, 50, 80, 80);
+  }
 }
 
-function mousePressed() {
+function keyPressed() {
     env.play();
 }
