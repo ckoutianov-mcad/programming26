3f948a17bb44231c5ae9aeae4b8e9e44e2544b99
ckoutianov <ckoutianov@mcad.edu>
2026-04-19 20:12:09 -0700

    key enabling updated instrument.js

--- week-12/instrument12a/instrument.js
+++ week-12/instrument12a/instrument.js
@@ -2,6 +2,7 @@ let osc, env;
 let notes = [50, 52, 54, 55, 57, 59, 61, 62];//notes
 let c = [0, 30, 60, 90, 175, 205, 270, 300]; //color
 let keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k']; //keys pressed
+let audioStarted = false;
 
 function setup() {
   createCanvas(800, 300);
@@ -24,6 +25,13 @@ function draw() {
   textAlign(CENTER);
   text("Press your keyboard to play", width/2, 30);
 
+  //audio warning if audio hasn't started
+  if(!audioStarted) {
+    fill(255, 0 , 0);
+    textSize(12);
+    text("Click here or press any key to enable sound", width/2, 200);
+  }
+
   //keys, when pressed
   for (let i = 0; i < 8; i++) {
     fill(0, 0, 100); //default key colors
@@ -41,5 +49,25 @@ function draw() {
 }
 
 function keyPressed() {
-    env.play();
+  //start audio on first key pressed
+  if (!audioStarted) {
+    userStartAudio();
+    audioStarted = true;
+    osc.amp(env);
+    console.log("Audio started!");
+  }
+  for(let i=0; i <keys.length; i++){
+    if (key == keys[i]) {
+      env.play();
+    }
+  }
+}
+
+function mousePressed() {
+  if (!audioStarted){
+    userStartAudio();
+    audioStarted = true;
+    osc.amp(env);
+    console.log("Audio started");
+  }
 }
