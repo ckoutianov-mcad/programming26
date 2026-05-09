ce1a8e3c9dfd8a5f90c84015c5440c40a66fd327
ckoutianov <ckoutianov@mcad.edu>
2026-05-09 10:47:44 -0700

    buttons for UI, no audio

--- week-15/sketch.js
+++ week-15/sketch.js
@@ -48,6 +48,7 @@ function setup () {
     createCanvas(windowWidth, windowHeight);
 
     colorMode(HSB, 360, 100, 100, 1);
+    createButtons();
 }
 
 function draw() {
@@ -59,62 +60,64 @@ function drawWinampBackground() {
         let t = y/height;
         stroke(240, 30 + (1-t)*20, 5 + (1-t)*8);
     line(0, y, width, y);
-        // let hue = 240; //blue
-        // let sat = 30 + (1 - t) * 20; //30-50% saturation
-        // let bright = 5 + (1 - t) * 8; // 5-13% brightness (very dark)
-        // stroke(hue, sat, bright);
-        // line(0, y, width, y);
-    // }
-    
-    // push();
-    // blendMode(MULTIPLY);
-    // fill(0, 0, 0, 0.15);
-    // noStroke();
-    // ellipse(width/2, height/2, width * 0.8, height * 0.8);
-    // pop();
     }
 }
 
-// function createButtons() {
-//     let menu = createDiv('');
-//     menu.style('position', 'fixed');
-//     menu.style('bottom', "80px");
-//     menu.style("left", "0");
-//     menu.style("right", "0");
-//     menu.style("display", "flex");
-//     menu.style("justify-content", "center");
-//     menu.style("gap", "15px");
-//     menu.style("z-index", "100");
-
-//     let moods = ['sad', 'neutral', 'happy'];
-
-//     for(let i = 0; i < moods.length; i++) {
-//         let mood = moods[i];
-//         let data = moodPresets[mood];
-//         let track = moodSongs[mood];
+function createButtons() {
+    let menu = createDiv('');
+    menu.style('position', 'fixed');
+    menu.style('bottom', '80px');
+    menu.style('left', '0');
+    menu.style('right', '0');
+    menu.style('display', 'flex');
+    menu.style('justify-content', 'center');
+    menu.style('gap', '15px');
+    menu.style('z-index', '100');
+    
+    let moods = ['sad', 'neutral', 'happy'];
+    
+    for (let i = 0; i < moods.length; i++) {
+        let mood = moods[i];
+        let data = moodPresets[mood];
+        let track = moodSongs[mood];
         
-//         let btnText = data.name + track.composer;
-//         let btn = createButton(btnText);
-//         btn.parent(menu);
-//         btn.style('padding', '10px 18px');
-//         btn.style("border", "none");
-//         btn.style("font-size", "12px");
-//         btn.style('color', 'white');
+        // Button text: Mood name + composer
+        let btnText = data.name + "\n" + track.composer;//new line
+        let btn = createButton(btnText);
+        btn.parent(menu);
+        btn.style('padding', '10px 18px');
+        btn.style('border-radius', '30px');
+        btn.style('border', 'none');
+        btn.style('cursor', 'pointer');
+        btn.style('font-size', '12px');
+        btn.style('font-weight', 'bold');
+        btn.style('color', 'white');
+        btn.style('line-height', '1.3');
+        btn.style('white-space', 'pre-line');
+        
+        // Button colors
+        if (mood === 'sad') btn.style('background', '#2752b0');
+        else if (mood === 'neutral') btn.style('background', '#2d623e');
+        else btn.style('background', '#bf9b39');
+        
+        btn.mousePressed(function() {
+            loadAndPlayMood(mood);
+        });
+    }
+    
+    playPauseBtn = createButton('Play');
+    playPauseBtn.parent(menu);
+    playPauseBtn.style('padding', '10px 24px');
+    playPauseBtn.style('border-radius', '30px');
+    playPauseBtn.style('border', 'none');
+    playPauseBtn.style('background', '#2c3e50');
+    playPauseBtn.style('color', '#00ff88');
+    playPauseBtn.style('cursor', 'pointer');
+    playPauseBtn.style('font-size', '14px');
+    playPauseBtn.style('font-weight', 'bold');
+    
+    playPauseBtn.mousePressed(togglePlayPause);
+}
 
-//         btn.mousePressed(function() {loadAndPlayMood(mood);
-//         });
-//     }
 
-//     playPauseBtn = createButton('Play');
-//     playPauseBtn.parent(menu);
-//     playPauseBtn.style('padding', '10px 24px');
-//     playPauseBtn.style('border-radius', '30px');
-//     playPauseBtn.style('border', 'none');
-//     playPauseBtn.style('background', '#000000');
-//     playPauseBtn.style('color', '#00ff88');
-//     playPauseBtn.style('font-size', '14px');
-//     playPauseBtn.style('font-weight', 'bold');
-
-//     playPauseBtn.mousePressed(togglePlayPause);
-// }
 
